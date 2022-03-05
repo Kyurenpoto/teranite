9eea7bd7648052cb76852c3efcda2eb9ed0f7209
Kyurenpoto <heal9179@gmail.com>
2022-03-05 21:51:44 +0900

    Change form & button to link

--- frontend/src/routes/LoginPage.svelte
+++ frontend/src/routes/LoginPage.svelte
@@ -1,8 +1,9 @@
 <section class="w-full h-[calc(100vh-240px)] my-10 flex flex-col gap-y-10 items-center justify-center bg-fuchsia-700">
   <div class="w-[var(--grid-column-2)] h-20 grid place-items-center bg-red-500">Sign In</div>
-  <form method="post" action="/auth/github">
-    <button type="submit" class="w-[var(--grid-column-4)] h-[60px] grid place-items-center bg-red-500" onclick="">
-      Github
-    </button>
-  </form>
+  <a
+    class="w-[var(--grid-column-4)] h-[60px] grid place-items-center bg-red-500"
+    href="https://github.com/login/oauth/authorize?client_id=Iv1.886232f678ac9a33&redirect_uri=http://127.0.0.1:8080/login/github/callback"
+  >
+    Github
+  </a>
 </section>
