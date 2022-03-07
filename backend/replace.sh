d8e25d485641eb8e4b0ae407d39608cf17099538
Kyurenpoto <heal9179@gmail.com>
2022-03-08 03:25:22 +0900

    Apply url_for so that starlette loads static files

---
+++ backend/replace.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sed "s/href=\"\/dist\/index.css\"/href=\"{{ url_for(\'static\', path=\'\/dist\/index.css\') }}\"/g" ../backend/static/index.html > tmp.txt
+\cp -f tmp.txt ../backend/static/index.html
+rm tmp.txt
+sed "s/src=\"\/dist\/index.js\"/src=\"{{ url_for(\'static\', path=\'\/dist\/index.js\') }}\"/g" ../backend/static/index.html > tmp.txt
+\cp -f tmp.txt ../backend/static/index.html
+rm tmp.txt
