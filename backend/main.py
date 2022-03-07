a7a19cde74e6ae11f26b6abe8d48864380a34d36
Kyurenpoto <heal9179@gmail.com>
2022-03-08 00:23:11 +0900

    Fix routing bug
    Modify args of app.mount()

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,24 @@
-import os
-
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "localhost:8080"],
+    allow_origins=["http://localhost:8000", "localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="static")
 
 
-@app.get("/{rest_of_path:path}")
-async def index(request: Request, rest_of_path: str):
-    print(f"rest_of_path: {rest_of_path}")
+@app.get("/{full_path:path}")
+async def index(request: Request, full_path: str):
+    print(f"full_path: {full_path}")
     return templates.TemplateResponse("index.html", {"request": request})
