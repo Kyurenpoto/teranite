948721b6257ed7f34085bad4a5227059e15d9169
Kyurenpoto <heal9179@gmail.com>
2022-03-08 08:23:12 +0000

    Write logics to get access token

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,10 @@ from typing import NamedTuple
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from httpx import AsyncClient, Response
 
 app = FastAPI()
 
@@ -29,17 +31,32 @@ class ExecutableRequest(ABC):
         pass
 
 
-class GithubLoginLogic(ExecutableRequest):
+class GithubData(NamedTuple):
+    clientId: str
+    clientSecret: str
+
+
+class GithubLoginLogic(GithubData, ExecutableRequest):
     async def execute(self, request: Request):
-        print(request.query_params["code"])
+        print("github logic")
+        async with AsyncClient() as client:
+            response: Response = await client.post(
+                url=f"https://github.com/login/oauth/access_token?client_id={self.clientId}&client_secret={self.clientSecret}&code={request.query_params['code']}",
+                headers={"Content-Type": "application/json"},
+                timeout=1.0,
+            )
+            print(response.text)
 
 
-logics: dict[str, ExecutableRequest] = {"login/github/callback": GithubLoginLogic()}
+logics: dict[str, ExecutableRequest] = {
+    "login/github/callback": GithubLoginLogic("", "")
+}
 
 
 @app.get("/{full_path:path}")
 async def index(request: Request, full_path: str):
     print(f"full_path: {full_path}")
     if full_path in logics:
-        return await logics[full_path].execute(request)
+        await logics[full_path].execute(request)
+        return RedirectResponse("http://127.0.0.1:8000/")
     return templates.TemplateResponse("index.html", {"request": request})
