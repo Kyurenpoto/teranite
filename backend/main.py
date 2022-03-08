3ee08a3bbcc454a9e3cf93ab74c399c71a9b4453
Kyurenpoto <heal9179@gmail.com>
2022-03-08 02:54:54 +0000

    Extract oauth logic

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import NamedTuple
+
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -18,7 +23,23 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="static")
 
 
+class ExecutableRequest(ABC):
+    @abstractmethod
+    async def execute(self, request: Request):
+        pass
+
+
+class GithubLoginLogic(ExecutableRequest):
+    async def execute(self, request: Request):
+        print(request.query_params["code"])
+
+
+logics: dict[str, ExecutableRequest] = {"login/github/callback": GithubLoginLogic()}
+
+
 @app.get("/{full_path:path}")
 async def index(request: Request, full_path: str):
     print(f"full_path: {full_path}")
+    if full_path in logics:
+        return await logics[full_path].execute(request)
     return templates.TemplateResponse("index.html", {"request": request})
