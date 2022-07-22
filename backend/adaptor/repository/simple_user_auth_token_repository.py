e843f68d5349dfecaab00affae1f9090336585f2
Kyurenpoto <heal9179@gmail.com>
2022-07-22 03:27:03 +0000

    Implement specifications of repository

--- backend/adaptor/repository/simple_user_auth_token_repository.py
+++ backend/adaptor/repository/simple_user_auth_token_repository.py
@@ -1,16 +1,30 @@
+from adaptor.repository.user_auth_token_repository import InvalidEmailError, UserAuthTokenRepository
+from dependencies.dependency import provider
 from entity.auth_token import OwnAuthToken, SocialAuthToken
+from entity.raw_datetime import RawDatetime
 from entity.user_auth_token import UserAuthToken
-
-from adaptor.repository.user_auth_token_repository import UserAuthTokenRepository
 
 
 class SimpleUserAuthTokenRepository(UserAuthTokenRepository):
+    def __init__(self):
+        self.datasource = provider["login"]["user-auth-token-source"]
+        self.datatimeGenerator = provider["login"]["raw-datatime-gen"]
+
     async def readByEmail(self, email: str) -> UserAuthToken:
-        pass
+        return await self.datasource.readByEmail(email)
 
     async def saveSocialAuthTokenByEmail(self, email: str, socialAuthToken: SocialAuthToken, socialType: str) -> None:
-        pass
+        try:
+            await self.readByEmail(email)
+
+            await self.datasource.updateSocialAuthTokenByEmail(email, socialAuthToken, socialType)
+        except InvalidEmailError:
+            await self.datasource.createSocialAuthTokenByEmail(email, socialAuthToken, socialType)
 
     async def saveOwnAuthTokenByEmail(self, email: str, ownAuthToken: OwnAuthToken) -> None:
-        pass
+        try:
+            await self.readByEmail(email)
 
+            await self.datasource.updateOwnAuthTokenByEmail(email, ownAuthToken, await self.datatimeGenerator.now())
+        except InvalidEmailError:
+            await self.datasource.createOwnAuthTokenByEmail(email, ownAuthToken, await self.datatimeGenerator.now())
